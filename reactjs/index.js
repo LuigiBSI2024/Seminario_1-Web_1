66b7f788ec943deb4ee279f0df8e8a54dfaa05c0
LuigiBSI2024 <laa10@aluno.ifnmg.edu.br>
2025-07-27 14:38:21 +0000

    Ajustes 2

--- reactjs/index.js
+++ reactjs/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./app";
+import App from "./listen";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
